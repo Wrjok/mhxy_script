613a0c6d5499050d45995a91d7d4de5c4102e423
Wrjok <958999527@qq.com>
2024-04-15 15:48:18 +0800

    bug

--- mhxy_menpai.py
+++ mhxy_menpai.py
@@ -2,47 +2,26 @@ from mhxy import *
 
 
 class Menpai:
-    def menpai(self, chaseWin):
-        def start():
-            return pyautogui.locateCenterOnScreen(r'resources/menpai/start.png')
-
-        # 流程任务 领取任务开始
-        def do():
-            def reach():
-                return pyautogui.locateCenterOnScreen(r'resources/menpai/select.png')
-
-            pyautogui.doubleClick(chaseWin[0], chaseWin[1])
-            startPos = start()
-            reachPos = None
-            if startPos is None:
-                reachPos = reach()
-            else:
-                pyautogui.leftClick(startPos.x, startPos.y)
-                pyautogui.doubleClick(chaseWin[0], chaseWin[1])
-            while reachPos is None and startPos is None:
-                reachPos = reach()
-                if reachPos is None:
-                    startPos = start()
-                cooldown(1)
-            if reachPos is not None:
-                pyautogui.leftClick(reachPos.x, reachPos.y)
-            else:
-                pyautogui.leftClick(startPos.x, startPos.y)
-                pyautogui.doubleClick(chaseWin[0], chaseWin[1])
-                while reachPos is None:
-                    reachPos = reach()
-                    cooldown(0.5)
-                pyautogui.leftClick(reachPos.x, reachPos.y)
-
-        startPos = start()
-        if startPos is not None:
-            pyautogui.leftClick(startPos.x, startPos.y)
-        escapeBattleDo(do)
-
+    def menpai(self):
+        flag = True
+        t = datetime.datetime.now().timestamp()
+        while flag:
+            # closePopupWindow()
+            print("开始检查是否进入战斗--------")
+            battle_flag = Util.locateCenterOnScreen(r'resources/menpai/battle.png')
+            if battle_flag is None:
+                clickIconPic(r'resources/menpai/task.png', 7)
+                clickIconPic(r'resources/menpai/task.png', 7)
+                clickIconPic(r'resources/menpai/enterBattle.png', 10)
+            cooldown(8)
+            t2 = datetime.datetime.now().timestamp()
+            if t2 - t > 60*60:
+                print("门店定时任务结束---------")
+                flag = False
 
 # 副本 进入第一个副本为起点
 if __name__ == '__main__':
     pyautogui.PAUSE = 0.5
     print("start task....")
     init()
-    Menpai().menpai((winRelativeX(-3), winRelativeY(5.8 + 0)))
+    Menpai().menpai()
